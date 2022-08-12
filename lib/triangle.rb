ff747b9694f98dee1d14c24c8f72101c68841a15
Michael Dear <michaelcdear@gmail.com>
2022-08-12 13:47:34 -0600

    commit tree a07b68c555f81f04e9bf287d1bf4857644603c52

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if self.validate == true
+      return :equilateral if @a == @b && @b == @c
+      return :isosceles if @a == @b || @b == @c || @a == @c
+      return :scalene
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid Triangle"
+    end
+  end
+
+  private
+
+  def validate
+    (@a > 0 && @b > 0 && @c > 0) && (@a + @b > @c && @a + @c > @b && @b + @c > @a)
+  end
+
 end
